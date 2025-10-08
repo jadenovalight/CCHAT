15b446e9723e744d43183b7cad984de540d8ee88
jadenovalight <jadenovalight@gmail.com>
2025-10-08 12:24:49 +0200

    simplify client and server, remove unnecessary

--- client.erl
+++ client.erl
@@ -1,7 +1,7 @@
 -module(client).
 -export([handle/2, initial_state/3]).
 
-% Client state record (保持原框架的简单结构)
+% Client state record
 -record(client_st, {
     gui,      % atom of the GUI process
     nick,     % nick/username of the client
@@ -20,26 +20,20 @@ initial_state(Nick, GUIAtom, ServerAtom) ->
 
 % Handle channel join request
 handle(St, {join, Channel}) ->
-    ServerAtom = St#client_st.server,
-    Nick = St#client_st.nick,
-    ClientPid = self(),  % 使用客户端进程自己的 PID
-    
-    % Check if already joined to this channel
     case lists:keyfind(Channel, 1, St#client_st.channels) of
         {Channel, _ChannelPid} ->
             {reply, {error, user_already_joined, "You have already joined this channel"}, St};
         false ->
             try
-                % Request server to join channel
-                case genserver:request(ServerAtom, {join, Channel, ClientPid, Nick}, 1000) of
+                case genserver:request(St#client_st.server, 
+                                     {join, Channel, self(), St#client_st.nick}, 1000) of
                     {ok, ChannelPid} ->
-                        % Successfully joined - update channel list
                         NewChannels = [{Channel, ChannelPid} | St#client_st.channels],
                         {reply, ok, St#client_st{channels = NewChannels}};
                     {error, user_already_joined} ->
                         {reply, {error, user_already_joined, "You have already joined this channel"}, St};
                     _ ->
-                        {reply, {error, server_not_reached, "Server error"}, St}
+                        {reply, {error, server_not_reached, "Cannot reach server"}, St}
                 end
             catch
                 _:_ ->
@@ -49,62 +43,43 @@ handle(St, {join, Channel}) ->
 
 % Handle channel leave request
 handle(St, {leave, Channel}) ->
-    % Check if client is in this channel
     case lists:keyfind(Channel, 1, St#client_st.channels) of
         false ->
             {reply, {error, user_not_joined, "You are not in this channel"}, St};
         {Channel, ChannelPid} ->
-            ClientPid = self(),
-            % Remove channel from client's list
             NewChannels = lists:keydelete(Channel, 1, St#client_st.channels),
-            
-            % Attempt to notify channel of departure (ignore failures)
             try
-                genserver:request(ChannelPid, {leave, ClientPid}, 1000)
+                genserver:request(ChannelPid, {leave, self()}, 1000)
             catch
                 _:_ -> ok
             end,
-            
             {reply, ok, St#client_st{channels = NewChannels}}
     end;
 
 % Handle message sending to a channel
-% 保持你原有的完整判断逻辑
 handle(St, {message_send, Channel, Msg}) ->
-    % Check if client is in the channel
     case lists:keyfind(Channel, 1, St#client_st.channels) of
         false ->
             % Not in channel - check with server if channel exists
-            ServerAtom = St#client_st.server,
-            Nick = St#client_st.nick,
-            ClientPid = self(),
-            
             try
-                % Attempt to send message through server
-                Result = genserver:request(ServerAtom, {message, Channel, Nick, Msg, ClientPid}, 1000),
+                Result = genserver:request(St#client_st.server, 
+                                         {message, Channel, St#client_st.nick, Msg, self()}, 1000),
                 case Result of
                     {error, user_not_joined} ->
-                        % Server confirms channel exists but client isn't joined
                         {reply, {error, user_not_joined, "You are not in this channel"}, St};
                     {error, channel_not_found} ->
-                        % Server reports channel doesn't exist
                         {reply, {error, server_not_reached, "Cannot reach server"}, St};
                     _ ->
-                        % Generic error fallback
                         {reply, {error, user_not_joined, "You are not in this channel"}, St}
                 end
             catch
                 _:_ ->
-                    % Unable to communicate with server
                     {reply, {error, server_not_reached, "Cannot reach server"}, St}
             end;
         {Channel, ChannelPid} ->
             % Already in channel - send message directly
-            Nick = St#client_st.nick,
-            ClientPid = self(),
-            
             try
-                case genserver:request(ChannelPid, {message, Nick, Msg, ClientPid}, 1000) of
+                case genserver:request(ChannelPid, {message, St#client_st.nick, Msg, self()}, 1000) of
                     ok ->
                         {reply, ok, St};
                     _ ->
@@ -112,24 +87,19 @@ handle(St, {message_send, Channel, Msg}) ->
                 end
             catch
                 _:_ ->
-                    % Handle channel unreachable scenarios (e.g., server shutdown)
                     {reply, {error, server_not_reached, "Cannot reach server"}, St}
             end
     end;
 
 % Handle nickname change request
 handle(St, {nick, NewNick}) ->
-    ServerAtom = St#client_st.server,
-    OldNick = St#client_st.nick,
-    
-    % No change needed if new nickname matches current
     case NewNick of
-        OldNick ->
+        OldNick when OldNick =:= St#client_st.nick ->
             {reply, ok, St};
         _ ->
             try
-                % Request server to change nickname
-                case genserver:request(ServerAtom, {nick, OldNick, NewNick}, 1000) of
+                case genserver:request(St#client_st.server, 
+                                     {nick, St#client_st.nick, NewNick}, 1000) of
                     ok ->
                         {reply, ok, St#client_st{nick = NewNick}};
                     {error, nick_taken} ->
@@ -139,7 +109,6 @@ handle(St, {nick, NewNick}) ->
                 end
             catch
                 _:_ ->
-                    % On server communication failure, locally update nickname
                     {reply, ok, St#client_st{nick = NewNick}}
             end
     end;
--- server.erl
+++ server.erl
@@ -1,30 +1,19 @@
 -module(server).
 -export([start/1, stop/1]).
 
-% Server state record
--record(server_st, {
-    channels = [],  % List of {ChannelName, ChannelPid} tuples
-    nicks = []      % List of registered nicknames
-}).
-
-% Channel state record
--record(channel_st, {
-    name,           % Name of the channel
-    clients = []    % List of ClientPid only (不存储昵称)
-}).
+% State: List of channel names (strings)
 
 % Starts a new server process registered under the given atom
 start(ServerAtom) ->
-    InitialState = #server_st{},
-    genserver:start(ServerAtom, InitialState, fun handle/2).
+    genserver:start(ServerAtom, [], fun handle/2).
 
 % Stops the server and all associated channels
 stop(ServerAtom) ->
     try
-        State = genserver:request(ServerAtom, get_state, 1000),
-        lists:foreach(fun({_Channel, ChannelPid}) ->
-            ChannelPid ! stop
-        end, State#server_st.channels)
+        Channels = genserver:request(ServerAtom, get_state, 1000),
+        lists:foreach(fun(Channel) ->
+            genserver:stop(list_to_atom(Channel))
+        end, Channels)
     catch
         _:_ -> ok
     end,
@@ -32,123 +21,143 @@ stop(ServerAtom) ->
     ok.
 
 % Handle join requests
-handle(State, {join, Channel, ClientPid, Nick}) ->
-    % Add nickname to server registry if it's new
-    NewNicks = case lists:member(Nick, State#server_st.nicks) of
-        false -> [Nick | State#server_st.nicks];
-        true -> State#server_st.nicks
-    end,
-    
-    % Check if channel already exists
-    case lists:keyfind(Channel, 1, State#server_st.channels) of
-        {Channel, ChannelPid} ->
-            % Join existing channel
-            case genserver:request(ChannelPid, {join, ClientPid}) of
+handle(Channels, {join, Channel, ClientPid, _Nick}) ->
+    case lists:member(Channel, Channels) of
+        true ->
+            % Channel exists, join it
+            case genserver:request(list_to_atom(Channel), {join, ClientPid}) of
                 ok ->
-                    {reply, {ok, ChannelPid}, State#server_st{nicks = NewNicks}};
+                    {reply, {ok, list_to_atom(Channel)}, Channels};
                 {error, already_joined} ->
-                    {reply, {error, user_already_joined}, State#server_st{nicks = NewNicks}}
+                    {reply, {error, user_already_joined}, Channels}
             end;
         false ->
-            % Create new channel
-            ChannelState = #channel_st{
-                name = Channel,
-                clients = [ClientPid]  % 只存储 ClientPid
-            },
-            ChannelPid = spawn(fun() -> channel_loop(ChannelState) end),
-            NewChannels = [{Channel, ChannelPid} | State#server_st.channels],
-            {reply, {ok, ChannelPid}, State#server_st{channels = NewChannels, nicks = NewNicks}}
+            % Create new channel using genserver
+            genserver:start(list_to_atom(Channel), [ClientPid], fun handle_channel/2),
+            NewChannels = [Channel | Channels],
+            {reply, {ok, list_to_atom(Channel)}, NewChannels}
     end;
 
-% Handle message requests (保持你的判断逻辑)
-handle(State, {message, Channel, Nick, Msg, ClientPid}) ->
-    case lists:keyfind(Channel, 1, State#server_st.channels) of
-        {Channel, ChannelPid} ->
-            case genserver:request(ChannelPid, {message, Nick, Msg, ClientPid}) of
+% Handle message requests
+handle(Channels, {message, Channel, Nick, Msg, ClientPid}) ->
+    case lists:member(Channel, Channels) of
+        true ->
+            case genserver:request(list_to_atom(Channel), {message, Nick, Msg, ClientPid}) of
                 ok ->
-                    {reply, ok, State};
+                    {reply, ok, Channels};
                 {error, not_joined} ->
-                    {reply, {error, user_not_joined}, State}
+                    {reply, {error, user_not_joined}, Channels}
             end;
         false ->
-            {reply, {error, channel_not_found}, State}
+            {reply, {error, channel_not_found}, Channels}
     end;
 
 % Handle nickname change requests
-handle(State, {nick, OldNick, NewNick}) ->
-    case lists:member(NewNick, State#server_st.nicks) of
+handle(Channels, {nick, _OldNick, NewNick}) ->
+    % Check if new nick is already in use in any channel
+    IsTaken = lists:any(fun(Channel) ->
+        try
+            case genserver:request(list_to_atom(Channel), {check_nick, NewNick}, 1000) of
+                nick_taken -> true;
+                ok -> false
+            end
+        catch
+            _:_ -> false
+        end
+    end, Channels),
+    
+    case IsTaken of
         true ->
-            {reply, {error, nick_taken}, State};
+            {reply, {error, nick_taken}, Channels};
         false ->
-            NewNicks = [NewNick | lists:delete(OldNick, State#server_st.nicks)],
-            % 不需要通知频道更新昵称，因为频道不存储昵称
-            {reply, ok, State#server_st{nicks = NewNicks}}
+            {reply, ok, Channels}
     end;
 
 % Get state (for administrative purposes)
-handle(State, get_state) ->
-    {reply, State, State};
+handle(Channels, get_state) ->
+    {reply, Channels, Channels};
 
 % Health check
-handle(State, ping) ->
-    {reply, pong, State};
+handle(Channels, ping) ->
+    {reply, pong, Channels};
 
 % Unknown request
-handle(State, _) ->
-    {reply, {error, unknown_request}, State}.
+handle(Channels, _) ->
+    {reply, {error, unknown_request}, Channels}.
 
-% Channel loop - 使用 genserver 风格但简化版
-% (如果要完全使用 genserver，可以改用 genserver:start，但这样也可以)
-channel_loop(State) ->
-    receive
-        {request, From, Ref, Request} ->
-            case Request of
-                {join, ClientPid} ->
-                    case lists:member(ClientPid, State#channel_st.clients) of
-                        true ->
-                            From ! {result, Ref, {error, already_joined}},
-                            channel_loop(State);
-                        false ->
-                            NewClients = [ClientPid | State#channel_st.clients],
-                            From ! {result, Ref, ok},
-                            channel_loop(State#channel_st{clients = NewClients})
-                    end;
-                
-                {leave, ClientPid} ->
-                    case lists:member(ClientPid, State#channel_st.clients) of
-                        true ->
-                            NewClients = lists:delete(ClientPid, State#channel_st.clients),
-                            From ! {result, Ref, ok},
-                            channel_loop(State#channel_st{clients = NewClients});
-                        false ->
-                            From ! {result, Ref, {error, not_joined}},
-                            channel_loop(State)
-                    end;
-                
-                {message, Nick, Msg, SenderPid} ->
-                    case lists:member(SenderPid, State#channel_st.clients) of
-                        true ->
-                            % Broadcast message to all clients except sender
-                            lists:foreach(fun(ClientPid) ->
-                                if 
-                                    ClientPid =/= SenderPid ->
-                                        ClientPid ! {request, self(), make_ref(), 
-                                                    {message_receive, State#channel_st.name, Nick, Msg}};
-                                    true -> ok
-                                end
-                            end, State#channel_st.clients),
-                            From ! {result, Ref, ok},
-                            channel_loop(State);
-                        false ->
-                            From ! {result, Ref, {error, not_joined}},
-                            channel_loop(State)
-                    end;
-                
-                _ ->
-                    From ! {result, Ref, {error, unknown_request}},
-                    channel_loop(State)
-            end;
-        
-        stop ->
-            ok
-    end.
+
+% ============================================================================
+% Channel handler function
+% ============================================================================
+
+% Handle join request
+handle_channel(Clients, {join, ClientPid}) ->
+    case lists:member(ClientPid, Clients) of
+        true ->
+            {reply, {error, already_joined}, Clients};
+        false ->
+            NewClients = [ClientPid | Clients],
+            {reply, ok, NewClients}
+    end;
+
+% Handle leave request
+handle_channel(Clients, {leave, ClientPid}) ->
+    case lists:member(ClientPid, Clients) of
+        true ->
+            NewClients = lists:delete(ClientPid, Clients),
+            {reply, ok, NewClients};
+        false ->
+            {reply, {error, not_joined}, Clients}
+    end;
+
+% Handle message broadcast
+handle_channel(Clients, {message, Nick, Msg, SenderPid}) ->
+    case lists:member(SenderPid, Clients) of
+        true ->
+            ChannelAtom = process_info(self(), registered_name),
+            ChannelName = case ChannelAtom of
+                {registered_name, Name} -> 
+                    atom_to_list(Name);
+                _ -> 
+                    "unknown"
+            end,
+            
+            spawn(fun() ->
+                lists:foreach(fun(ClientPid) ->
+                    if 
+                        ClientPid =/= SenderPid ->
+                            try
+                                genserver:request(ClientPid, {message_receive, ChannelName, Nick, Msg}, 1000)
+                            catch
+                                _:_ -> ok  % Ignore if client is unreachable
+                            end;
+                        true -> 
+                            ok
+                    end
+                end, Clients)
+            end),
+            {reply, ok, Clients};
+
+        false ->
+            {reply, {error, not_joined}, Clients}
+    end;
+
+% Check if a nick is in use in this channel (for distinction assignment)
+handle_channel(Clients, {check_nick, NewNick}) ->
+    IsTaken = lists:any(fun(ClientPid) ->
+        try
+            CurrentNick = genserver:request(ClientPid, whoami, 1000),
+            CurrentNick =:= NewNick
+        catch
+            _:_ -> false
+        end
+    end, Clients),
+    
+    case IsTaken of
+        true -> {reply, nick_taken, Clients};
+        false -> {reply, ok, Clients}
+    end;
+
+% Unknown request
+handle_channel(Clients, _) ->
+    {reply, {error, unknown_request}, Clients}.
